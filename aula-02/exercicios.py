f71ea68a24b0ec471dea7b8749e61bb63e09732d
Joao Pedro <joaopo.0607@gmail.com>
2025-11-12 07:17:10 -0300

    exercicios booleanos

--- aula-02/exercicios.py
+++ aula-02/exercicios.py
@@ -87,7 +87,26 @@
 # #### Booleanos (`bool`)
 
 # 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+# valor1 = True
+# valor2 = False
+# print(valor1 and valor2)
+
 # 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+# valor1 = True
+# valor2 = False
+# print(valor1 or valor2)
+
 # 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+# valor1 = True
+# valor2 = False
+# print(not valor1)
+
 # 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+# valor1 = False
+# valor2 = False
+# print(valor1 == valor2)
+
 # 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+# valor1 = True
+# valor2 = True
+# print(valor1 != valor2)
