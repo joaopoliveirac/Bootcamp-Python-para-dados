d3d83d3e11349b3b40d70de8047989ca1b0e164a
Joao Pedro <joaopo.0607@gmail.com>
2025-11-12 07:13:20 -0300

    exercicios de string

--- aula-02/exercicios.py
+++ aula-02/exercicios.py
@@ -53,18 +53,36 @@
 # print(convertido)
 
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
-import math
-raio = float(input('informe o raio: '))
-area = math.pi * (raio ** 2)
-print(area)
+# import math
+# raio = float(input('informe o raio: '))
+# area = math.pi * (raio ** 2)
+# print(area)
 
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+# palavra = input('informe a string')
+# print(palavra.upper())
+
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+# nome = input('informe o nome completo : ')
+# print(nome.lower())
+
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+# frase = input('informe uma frase: ')
+# print(frase.strip())
+
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+# data = input('digite uma data no formato: "dd/mm/aaaa"')
+# data_formatada = data.split('/')
+# print(f'dia: {data_formatada[0]}')
+# print(f'mes: {data_formatada[1]}')
+# print(f'ano: {data_formatada[2]}')
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+# palavra1 = input('informe o primeiro nome: ')
+# palavra2 = input('informe o segundo nome: ')
+# print(palavra1 + palavra2)
 
 # #### Booleanos (`bool`)
 
