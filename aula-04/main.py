6f15f1a31a48403ec6748d9ffcedc3733e5430d6
Joao Pedro <joaopo.0607@gmail.com>
2025-11-19 07:18:00 -0300

    exercicios aula 04

---
+++ aula-04/main.py
@@ -0,0 +1,20 @@
+# 1- Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
+# 2- Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
+# 3- Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação.
+# 4- Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
+# 5- Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
+# 6- Eliminação de Duplicatas. Dada uma lista de emails, remover todos os duplicados.
+# 7- Filtragem de Dados. Dada uma lista de idades, filtrar apenas aquelas que são maiores ou iguais a 18.
+# 8- Ordenação Personalizada. Dada uma lista de dicionários representando pessoas, ordená-las pelo nome.
+# 9- Agregação de Dados. Dado um conjunto de números, calcular a média.
+# 10- Divisão de Dados em Grupos. Dada uma lista de valores, dividir em duas listas: uma para valores pares e outra para ímpares.
+# 11- Atualização de Dados. Dada uma lista de dicionários representando produtos, atualizar o preço de um produto específico.
+# 12- Fusão de Dicionários. Dados dois dicionários, fundi-los em um único dicionário.
+# 13- Filtragem de Dados em Dicionário. Dado um dicionário de estoque de produtos, filtrar aqueles com quantidade maior que 0.
+# 14- Extração de Chaves e Valores. Dado um dicionário, criar listas separadas para suas chaves e valores.
+# 15- Contagem de Frequência de Itens. Dada uma string, contar a frequência de cada caractere usando um dicionário.
+# 16- Escreva uma função que receba uma lista de números e retorne a soma de todos os números.
+# 17- Crie uma função que receba um número como argumento e retorne True se o número for primo e False caso contrário.
+# 18- Desenvolva uma função que receba uma string como argumento e retorne essa string revertida.
+# 19- Implemente uma função que receba dois argumentos: uma lista de números e um número. A função deve retornar todas as combinações de pares na lista que somem ao número dado.
+# 20- Escreva uma função que receba um dicionário e retorne uma lista de chaves ordenadas
