e166ae85c791a04ff7a48b2fb75d64ac4658dc9e
Joao Pedro <joaopo.0607@gmail.com>
2025-11-24 06:37:05 -0300

    subindo alguns exercicios da aula 03

--- aula-03/exercicios.py
+++ aula-03/exercicios.py
@@ -56,23 +56,85 @@ from datetime import time
 # transações suspeitas. Uma transação é considerada suspeita se o valor for superior 
 # a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
 # Dada uma transação como `transacao = {'valor': 12000, 'hora': 20}`, verifique se ela é suspeita.
-
-
+# transacao: dict[str, int]  = {'valor': 9000, 'hora': 21}
+# if transacao['valor'] > 10000 or transacao['hora'] < 9 or transacao['hora'] > 20:
+#     print('TRANSACAO SUSPEITA!')
+# else:
+#     print('transacao normal, pode seguir.')
 
 ### Exercício 6. Contagem de Palavras em Textos
 # Objetivo:** Dado um texto, contar quantas vezes cada palavra única aparece nele.
+# resolucao 1
+# texto = 'eu sou um cara que estuda dados em python, eu gosto de estudar todos os dias. eu amo esse cara, python, cara, teste, teste, teste'
+# texto = texto.lower()
+# texto = texto.replace(',', '')
+# texto = texto.split()
+# quantidade = {}
+# for palavra in texto:
+#     if palavra in quantidade:
+#         quantidade[palavra] += 1
+#     else:
+#         quantidade[palavra] = 1
+# print(quantidade)
+
+# resolucao 2, mais direta
+# from collections import Counter
+# texto = 'eu sou um cara que estuda dados em python, eu gosto de estudar todos os dias. eu amo esse cara, python, cara, teste, teste, teste'
+# texto = texto.lower()
+# for char in ",.!?;:":
+#     texto.replace(char, '')
+# quantidade = Counter(texto.split())
+# print(quantidade)
 
 ### Exercício 7. Normalização de Dados
 # Objetivo:** Normalizar uma lista de números para que fiquem na escala de 0 a 1.
+# import numpy as np
+# numeros = [10, 20, 30, 40]
+# min_val = min(numeros)  # 10
+# max_val = max(numeros)  # 40
+# normalizados = [(x - min_val) / (max_val - min_val) for x in numeros]
+# print(normalizados)
 
 ### Exercício 8. Filtragem de Dados Faltantes
 # Objetivo:** Dada uma lista de dicionários representando dados de usuários, filtrar aqueles que têm um campo específico faltando
+# usuarios = [
+#     {"nome": "João", "idade": 25, "email": "joao@email.com"},
+#     {"nome": "Maria", "idade": 30},  # faltando e-mail
+#     {"nome": "Ana", "idade": 22, "email": "ana@email.com"},
+#     {"nome": "Pedro"}]  # faltando idade e e-mail
+# campos_obrigatorios = ['nome', 'idade', 'email']
+
+# for usuario in usuarios:
+#     faltando = [campo for campo in campos_obrigatorios if campo not in usuario]
+#     if faltando:
+#         texto = ", ".join(faltando)
+#         print(f'O usuario {usuario['nome']} está com o campo {texto} incompleto.')
 
 ### Exercício 9. Extração de Subconjuntos de Dados
 # Objetivo:** Dada uma lista de números, extrair apenas aqueles que são pares.
+# numeros = [10,20,15,19,30,45,60]
+# pares = [num for num in numeros if (num % 2) == 0]
+# print(pares)
 
 ### Exercício 10. Agregação de Dados por Categoria
 # Objetivo:** Dado um conjunto de registros de vendas, calcular o total de vendas por categoria.
+# vendas = [
+#     {"id": 1, "produto": "Camisa", "quantidade": 3, "preco": 79.90, "vendedor": "Ana", "cidade": "São Paulo", "categoria": "Vestuário"},
+#     {"id": 2, "produto": "Tênis", "quantidade": 1, "preco": 299.90, "vendedor": "João", "cidade": "Rio de Janeiro", "categoria": "Calçados"},
+#     {"id": 3, "produto": "Calça Jeans", "quantidade": 2, "preco": 149.90, "vendedor": "Ana", "cidade": "São Paulo", "categoria": "Vestuário"},
+#     {"id": 4, "produto": "Boné", "quantidade": 5, "preco": 49.90, "vendedor": "Marcos", "cidade": "Belo Horizonte", "categoria": "Acessórios"},
+#     {"id": 5, "produto": "Meias", "quantidade": 10, "preco": 9.90, "vendedor": "João", "cidade": "Curitiba", "categoria": "Vestuário"},
+#     {"id": 6, "produto": "Camisa", "quantidade": 1, "preco": 79.90, "vendedor": "Ana", "cidade": "Rio de Janeiro", "categoria": "Vestuário"},
+#     {"id": 7, "produto": "Tênis", "quantidade": 2, "preco": 299.90, "vendedor": "Marcos", "cidade": "São Paulo", "categoria": "Calçados"},
+# ]
+
+# categorias = {v["categoria"] for v in vendas}
+# total = {categoria: sum(venda['quantidade'] * venda['preco'] for venda in vendas if venda['categoria'] == categoria)
+#          for categoria in categorias}
+
+# print(total)
+
+
 
 ### Exercícios com WHILE
 
